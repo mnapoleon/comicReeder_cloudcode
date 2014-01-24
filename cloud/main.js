e08f2fce853bba25110f5ac697af6bfd923c4993
Michael Napoleon <michael.napoleon@gmail.com>
2014-01-24 14:32:05 -0500

    changes to cloud code

--- cloud/main.js
+++ cloud/main.js
@@ -1,10 +1,23 @@
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
+Parse.Cloud.define("hello", function(request, response) {
+  var query = new Parse.Query("Comics");
+  query.find({
+    success: function(results) {
+      console.log(results[0]);
+      response.success("Hello world!");
+    },
+    error: function() {
+      response.error("no data");
+    }
+  }); 
+});
+
 Parse.Cloud.define("getComics", function(request, response) {
-  var query = new Parse.Query("Comic");
+  var query = new Parse.Query("Comics");
   query.find({
     success:  function(results) {
-      console.log(results[1].get("Writer"));
+      console.log(results.length);
       var data;
       for (comic in results) {
         data += comic;
@@ -19,13 +32,13 @@ Parse.Cloud.define("getComics", function(request, response) {
 });
  
 Parse.Cloud.define("getWriters", function(req,res) {
-  var query = new Parse.Query("Comic");
+  var query = new Parse.Query("Comics");
   query.find({
     success: function(results) {
       var writers = "";
       //for (comic in results) {
       for (var i=0; i< results.length; i++) {
-        writers = writers + "**" +  results[i].get("Writer");
+        writers = writers + "**" +  results[i].get("writer");
       }
       res.success(writers);
     },
